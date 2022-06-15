aa2326c3c71c97c721cf12589acccefa293b85d5
Lucas Parise <lucas.parise12@gmail.com>
2022-06-15 20:31:44 -0300

    entrega clase 2

---
+++ src/components/NavBar.js
@@ -0,0 +1,39 @@
+import './NavBar.css';
+import logo from "../assets/Icono.png";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function NavBar(){
+    return(
+        <header className='boxHeader sticky-top'>
+                    <nav className='navbar navbar-expand-lg navbar-light menu'>
+            <div className='container-fluid'>
+                <a className='navbar-brand' href='#'>
+                    <img src={logo} alt='Logo' height='75' width='auto'/>
+                </a>
+                <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+                <div className='collapse navbar-collapse  justify-content-end' id='navbarNav'>
+                    <ul className='navbar-nav'>
+                        <li className='nav-item itemMenu'>
+                            <a className='nav-link' href=''>Productos</a>
+                        </li>
+                        <li className='nav-item itemMenu'>
+                            <a className='nav-link' href=''>Carrito</a>
+                        </li>
+                        <li className='nav-item itemMenu'>
+                            <a className='nav-link' href=''>Preguntas Frecuentes</a>
+                        </li>
+                        <li className='nav-item itemMenu'>
+                            <a className='nav-link' href=''>Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        </header>
+    )
+}
+
+export default NavBar;
